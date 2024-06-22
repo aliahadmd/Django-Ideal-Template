4f99d155b8b26eaa058b5728eab5176e0387b665
Ali <aliahadmd1@gmail.com>
2024-06-22 16:35:46 +0000

    change log dir

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,5 +2,5 @@ import multiprocessing
 
 bind = "127.0.0.1:8001"
 workers = multiprocessing.cpu_count() * 2 + 1
-accesslog = "/home/dev/app/Django-Ideal-Template/gunicorn-access.log"
-errorlog = "/home/dev/app/Django-Ideal-Template/gunicorn-error.log"
+accesslog = "/home/dev/app/Django-Ideal-Template/log/gunicorn-access.log"
+errorlog = "/home/dev/app/Django-Ideal-Template/log/gunicorn-error.log"
