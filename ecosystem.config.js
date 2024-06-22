ec212766b3f7cd2437a3d2f749af7b7b8689b40c
Ali <aliahadmd1@gmail.com>
2024-06-22 14:39:35 +0000

    ok

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [{
+      name: "django_app",
+      script: "/home/dev/app/Django-Ideal-Template/venv/bin/gunicorn",
+      args: "--config gunicorn_config.py core.wsgi:application",
+      cwd: "/home/dev/app/Django-Ideal-Template",
+      interpreter: "/home/dev/app/Django-Ideal-Template/venv/bin/python",
+    }]
+  }
---
+++ gunicorn_config.py
@@ -0,0 +1,6 @@
+import multiprocessing
+
+bind = "127.0.0.1:8000"
+workers = multiprocessing.cpu_count() * 2 + 1
+accesslog = "/home/dev/app/Django-Ideal-Template/gunicorn-access.log"
+errorlog = "/home/dev/app/Django-Ideal-Template/gunicorn-error.log"
